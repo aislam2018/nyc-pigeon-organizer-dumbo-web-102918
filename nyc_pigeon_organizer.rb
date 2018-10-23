4f5484efe2b9b1eb56687377dcd369a1ffdbd63c
anik1606 <anik1606@gmail.com>
2018-10-23 22:11:47 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -8,9 +8,10 @@ def nyc_pigeon_organizer(data)
           pigeon_list[name] = {}
           if !pigeon_list[name][category]
             pigeon_list[name][category] = []
+            binding.pry
             if pigeon_list[name] == name
               pigeon_list[name][category] << feature
-              binding.pry
+              
             end
           end
         end
