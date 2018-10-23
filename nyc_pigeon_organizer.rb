894fb2a9352478df6c87770c021993297f7a5962
anik1606 <anik1606@gmail.com>
2018-10-23 22:15:52 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,15 +4,9 @@ def nyc_pigeon_organizer(data)
   data.each do |category, subcat|
     subcat.each do |feature, names|
       names.each do |name|
-        if !pigeon_list[name]
           pigeon_list[name] ||= {}
-          if !pigeon_list[name][category]
             pigeon_list[name][category] ||= []
               pigeon_list[name][category] << feature
-              
-          
-          end
-        end
       end
     end
   end
